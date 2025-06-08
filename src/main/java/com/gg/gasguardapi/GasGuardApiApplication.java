197c4a2f6ba0e58509bb97f18f5d34cded550492
GerardoQuilla <g.quillaluyo@gmail.com>
2025-06-07 19:00:50 -0500

    initial commit

---
+++ src/main/java/com/gg/gasguardapi/GasGuardApiApplication.java
@@ -0,0 +1,13 @@
+package com.gg.gasguardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasGuardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GasGuardApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gg/gasguardapi/GasGuardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gg.gasguardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasGuardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
